3b6a4d7f928f7ad8a371ecf92c530b104500aaef
Ravencita <95237096+Ravycita@users.noreply.github.com>
2022-05-19 19:54:29 -0300

    Backend
    Creacion de carpeta Backend

---
+++ PortafolioBonziniMelody/Backend/src/main/java/com/portafolio/bonzinimelody/BonzinimelodyApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio.bonzinimelody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonzinimelodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonzinimelodyApplication.class, args);
+	}
+
+}
---
+++ PortafolioBonziniMelody/Backend/src/test/java/com/portafolio/bonzinimelody/BonzinimelodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.bonzinimelody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonzinimelodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
